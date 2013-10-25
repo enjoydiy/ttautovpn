dffa070c66923b3f9c0d7f295948e0e11a47bf69
zijiao <admin@enjoydiy.com>
2013-10-25 09:00:08 +0800

    The routes include china IPs,created on 2013/10/25

--- up.sh
+++ up.sh
@@ -1,4 +1,4 @@
-#2013/10/24 created by http://bbs.enjoydiy.com
+#2013/10/25 created by http://bbs.enjoydiy.com
 #QQ:58076972 E-mail:admin@enjoydiy.com
 route add -net 1.0.1.0 netmask 255.255.255.0 gw $OLDGW
 route add -net 1.0.2.0 netmask 255.255.254.0 gw $OLDGW
@@ -859,6 +859,7 @@ route add -net 103.251.236.0 netmask 255.255.252.0 gw $OLDGW
 route add -net 103.251.240.0 netmask 255.255.252.0 gw $OLDGW
 route add -net 103.252.28.0 netmask 255.255.252.0 gw $OLDGW
 route add -net 103.252.36.0 netmask 255.255.252.0 gw $OLDGW
+route add -net 103.252.64.0 netmask 255.255.252.0 gw $OLDGW
 route add -net 106.0.0.0 netmask 255.255.255.0 gw $OLDGW
 route add -net 106.0.2.0 netmask 255.255.254.0 gw $OLDGW
 route add -net 106.0.4.0 netmask 255.255.252.0 gw $OLDGW
