641a75457a3ff7de3c3dbcee359cc3e138750e2d
zijiao <admin@enjoydiy.com>
2015-07-03 09:00:05 +0800

    The routes include china IPs,created on 2015/07/03

--- down.sh
+++ down.sh
@@ -1,4 +1,4 @@
-#2015/07/02 created by http://bbs.enjoydiy.com
+#2015/07/03 created by http://bbs.enjoydiy.com
 #QQ:58076972 E-mail:admin@enjoydiy.com
 route del -net 1.0.1.0 netmask 255.255.255.0
 route del -net 1.0.2.0 netmask 255.255.254.0
@@ -1107,6 +1107,7 @@ route del -net 45.121.72.0 netmask 255.255.252.0
 route del -net 45.121.92.0 netmask 255.255.252.0
 route del -net 45.121.96.0 netmask 255.255.252.0
 route del -net 45.121.104.0 netmask 255.255.252.0
+route del -net 45.121.172.0 netmask 255.255.252.0
 route del -net 47.92.0.0 netmask 255.252.0.0
 route del -net 47.96.0.0 netmask 255.224.0.0
 route del -net 49.4.0.0 netmask 255.252.0.0
@@ -2298,6 +2299,7 @@ route del -net 103.61.172.0 netmask 255.255.252.0
 route del -net 103.61.176.0 netmask 255.255.252.0
 route del -net 103.61.184.0 netmask 255.255.252.0
 route del -net 103.61.188.0 netmask 255.255.252.0
+route del -net 103.62.24.0 netmask 255.255.252.0
 route del -net 103.224.40.0 netmask 255.255.252.0
 route del -net 103.224.44.0 netmask 255.255.252.0
 route del -net 103.224.60.0 netmask 255.255.252.0
