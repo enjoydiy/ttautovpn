d4f477719c3b5724dd9d45cb8cb83323b90431cf
zijiao <admin@enjoydiy.com>
2015-07-21 09:00:06 +0800

    The routes include china IPs,created on 2015/07/21

--- up.sh
+++ up.sh
@@ -1,4 +1,4 @@
-#2015/07/20 created by http://bbs.enjoydiy.com
+#2015/07/21 created by http://bbs.enjoydiy.com
 #QQ:58076972 E-mail:admin@enjoydiy.com
 route add -net 1.0.1.0 netmask 255.255.255.0 gw $OLDGW
 route add -net 1.0.2.0 netmask 255.255.254.0 gw $OLDGW
@@ -1194,6 +1194,12 @@ route add -net 45.123.244.0 netmask 255.255.252.0 gw $OLDGW
 route add -net 45.123.248.0 netmask 255.255.252.0 gw $OLDGW
 route add -net 45.123.252.0 netmask 255.255.252.0 gw $OLDGW
 route add -net 45.124.0.0 netmask 255.255.252.0 gw $OLDGW
+route add -net 45.124.20.0 netmask 255.255.252.0 gw $OLDGW
+route add -net 45.124.24.0 netmask 255.255.252.0 gw $OLDGW
+route add -net 45.124.28.0 netmask 255.255.252.0 gw $OLDGW
+route add -net 45.124.32.0 netmask 255.255.252.0 gw $OLDGW
+route add -net 45.124.36.0 netmask 255.255.252.0 gw $OLDGW
+route add -net 45.124.44.0 netmask 255.255.252.0 gw $OLDGW
 route add -net 47.92.0.0 netmask 255.252.0.0 gw $OLDGW
 route add -net 47.96.0.0 netmask 255.224.0.0 gw $OLDGW
 route add -net 49.4.0.0 netmask 255.252.0.0 gw $OLDGW
@@ -2470,6 +2476,12 @@ route add -net 103.192.100.0 netmask 255.255.252.0 gw $OLDGW
 route add -net 103.192.104.0 netmask 255.255.252.0 gw $OLDGW
 route add -net 103.192.108.0 netmask 255.255.252.0 gw $OLDGW
 route add -net 103.192.112.0 netmask 255.255.252.0 gw $OLDGW
+route add -net 103.192.128.0 netmask 255.255.252.0 gw $OLDGW
+route add -net 103.192.132.0 netmask 255.255.252.0 gw $OLDGW
+route add -net 103.192.136.0 netmask 255.255.252.0 gw $OLDGW
+route add -net 103.192.140.0 netmask 255.255.252.0 gw $OLDGW
+route add -net 103.192.144.0 netmask 255.255.252.0 gw $OLDGW
+route add -net 103.192.164.0 netmask 255.255.252.0 gw $OLDGW
 route add -net 103.224.40.0 netmask 255.255.252.0 gw $OLDGW
 route add -net 103.224.44.0 netmask 255.255.252.0 gw $OLDGW
 route add -net 103.224.60.0 netmask 255.255.252.0 gw $OLDGW
