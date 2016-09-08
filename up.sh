94f7d182a81742fd27a72d38967cd06a293edb31
zijiao <admin@enjoydiy.com>
2016-09-08 09:00:04 +0800

    The routes include china IPs,created on 2016/09/08

--- up.sh
+++ up.sh
@@ -1,4 +1,4 @@
-#2016/09/07 created by http://bbs.enjoydiy.com
+#2016/09/08 created by http://bbs.enjoydiy.com
 #QQ:58076972 E-mail:admin@enjoydiy.com
 route add -net 1.0.1.0 netmask 255.255.255.0 gw $OLDGW
 route add -net 1.0.2.0 netmask 255.255.254.0 gw $OLDGW
@@ -3183,6 +3183,7 @@ route add -net 103.74.48.0 netmask 255.255.252.0 gw $OLDGW
 route add -net 103.74.56.0 netmask 255.255.252.0 gw $OLDGW
 route add -net 103.74.60.0 netmask 255.255.252.0 gw $OLDGW
 route add -net 103.74.80.0 netmask 255.255.252.0 gw $OLDGW
+route add -net 103.74.124.0 netmask 255.255.252.0 gw $OLDGW
 route add -net 103.192.0.0 netmask 255.255.252.0 gw $OLDGW
 route add -net 103.192.4.0 netmask 255.255.252.0 gw $OLDGW
 route add -net 103.192.8.0 netmask 255.255.252.0 gw $OLDGW
@@ -5276,6 +5277,7 @@ route add -net 163.142.0.0 netmask 255.255.0.0 gw $OLDGW
 route add -net 163.177.0.0 netmask 255.255.0.0 gw $OLDGW
 route add -net 163.179.0.0 netmask 255.255.0.0 gw $OLDGW
 route add -net 163.204.0.0 netmask 255.255.0.0 gw $OLDGW
+route add -net 164.52.0.0 netmask 255.255.128.0 gw $OLDGW
 route add -net 166.111.0.0 netmask 255.255.0.0 gw $OLDGW
 route add -net 167.139.0.0 netmask 255.255.0.0 gw $OLDGW
 route add -net 167.189.0.0 netmask 255.255.0.0 gw $OLDGW
